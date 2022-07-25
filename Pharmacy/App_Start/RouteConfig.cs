7fdfc9980cbe1b1beb1d4e32d615ff27f0229fb4
Shakikul Islam <shakikulislam625010@gmail.com>
2022-07-26 02:00:09 +0600

    Add Template and update aspNetMvc and add genericName Controllar

--- Pharmacy/App_Start/RouteConfig.cs
+++ Pharmacy/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Pharmacy
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "GenericNames", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Pharmacy/Controllers/GenericNamesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Pharmacy.Controllers
+{
+    public class GenericNamesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
